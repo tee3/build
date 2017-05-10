9c0dfb38dd9b4f63e24097c820097e66c6dfd791
Thomas Brown <tabsoftwareconsulting@gmail.com>
2017-05-10 18:25:22 -0400

    types: Added a type for TeX.

---
+++ src/tools/types/tex.py
@@ -0,0 +1,10 @@
+# Copyright Thomas Brown 2017. Distributed under the Boost
+# Software License, Version 1.0. (See accompanying
+# file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+from b2.build import type
+
+def register ():
+    type.register_type ('TEX', ['tex'])
+
+register ()
